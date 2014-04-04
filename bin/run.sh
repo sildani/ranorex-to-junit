5db9d7409337ebdb10b12040afe1cf9897955095
Daniel Silva <daniel@silvanolte.com>
2014-04-03 22:34:09 -0400

    Fleshed out first implementation of Main. Added a run script.

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-groovy src/Main
+java -jar $*
--- src/Main.groovy
+++ src/Main.groovy
@@ -1,4 +1,14 @@
 class Main {
   public static void main(args) {
+    if (args.size() != 2) {
+      throw new IllegalArgumentException("Must pass two arguments - the relative location of the Ranorex " +
+        "test report file and the name of the file itself.")
+    }
+
+    def inputFile = new File(args[0] + File.separator + args[1])
+    def outputFile = new File(args[0] + File.separator + 'TESTS-TestSuites.xml').withWriter { out ->
+      out.write(new RanorexToJunitConverter().convert(inputFile.getText()))
+    }
+
   }
 }
